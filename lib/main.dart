ee77dc80253536cde4932869ac893164ab5cd1cd
Didik Maulana Ardiansyah <didikmaulana49@gmail.com>
2019-06-03 11:09:39 +0700

    add html parsed dependencies, finished quotes page

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import 'package:html/parser.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -13,6 +14,17 @@ void main() {
   ));
 }
 
+List<Quotes> parseQuotes(String responseBody) {
+    final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+    return parsed.map<Quotes>((json) => Quotes.fromJson(json)).toList();
+}
+
+Future<List<Quotes>> fetchQuotes(http.Client client) async {
+  final urlApi = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=10";
+  final response = await client.get(urlApi);
+  return compute(parseQuotes, response.body);
+}
+
 class QuotesPage extends StatefulWidget {
   @override
   _QuotesPageState createState() => _QuotesPageState();
@@ -20,49 +32,19 @@ class QuotesPage extends StatefulWidget {
 
 class _QuotesPageState extends State<QuotesPage> {
 
-  static const urlApi = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=10";
-
-  List<Quotes> parseQuotes(String responseBody) {
-    final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
-    return parsed.map<Quotes>((json) => Quotes.fromJson(json)).toList();
-  }
-
-  Future<List<Quotes>> fetchQuotes(http.Client client) async {
-    final response = await client.get(urlApi);
-    return compute(parseQuotes, response.body);
-  }
-
   @override
   Widget build(BuildContext context) {
     
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
 
-    Widget _buildRefetchButton = InkWell(
-      onTap: () {},
-      child: Container(
-        width: width,
-        margin: EdgeInsets.symmetric(
-          horizontal: width/10,
-        ),
-        padding: EdgeInsets.symmetric(
-          vertical: height/50,
-        ), 
-        child: Center(
-          child: Text(
-            'REFETCH QUOTES',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        decoration: BoxDecoration(
-          border: Border.all(
-            width: 2.0,
-            color: Colors.white,
-          ),
-          borderRadius: BorderRadius.circular(20.0),
+    Widget _titleSection = Center(
+      child: Text(
+        'Quotes Apps',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w300,
+          fontSize: 20.0,
         ),
       ),
     );
@@ -70,7 +52,8 @@ class _QuotesPageState extends State<QuotesPage> {
     return Scaffold(
       body: Container(
         padding: EdgeInsets.only(
-          bottom: height/20,
+          top: height/12,
+          bottom: height/5.5,
         ),
         decoration: BoxDecoration(
           gradient: LinearGradient(
@@ -81,23 +64,92 @@ class _QuotesPageState extends State<QuotesPage> {
         ),
         child: Column(
           children: [
-            Expanded(
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 5,
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: EdgeInsets.only(right: 50.0),
-                    width: 50.0,
-                    height: height/5,
-                    color: Colors.blue,
-                  );
-                },
+            _titleSection,
+            Padding(
+              padding: EdgeInsets.only(
+                top: height/12,
               ),
             ),
-            _buildRefetchButton,
+            FutureBuilder<List<Quotes>>(
+              future: fetchQuotes(http.Client()),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) print(snapshot.error);
+
+                if (snapshot.hasData) {
+                  return _buildQuotesSection(
+                    width,
+                    height,
+                    snapshot.data
+                  );
+                } else {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildQuotesSection(
+    final double width, 
+    final double height,
+    final List<Quotes> quotes
+  ) {
+
+    String _htmlParsed(String text) {
+      var document = parse(text);
+      String parsedString = parse(document.body.text).documentElement.text;
+      return parsedString;
+    }
+
+    return Expanded(
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: quotes.length,
+        itemBuilder: (context, index) {
+          return Container(
+            margin: EdgeInsets.only(
+              right: width/20,
+              left: width/20,
+            ),
+            padding: EdgeInsets.all(16.0),
+            width: width/1.1,
+            height: height/5,
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.15),
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            ),
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    _htmlParsed(quotes[index].content),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 18.0,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    quotes[index].title,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
